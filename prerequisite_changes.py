c4f70044169444beff0fc082fbc2535646e071b0
BhavikKothari-eaton <BhavikKothari@eaton.com>
2023-02-17 01:47:40 -0500

    Modified prerequisite_changes.py after running flake8
    Signed-off-by: BhavikKothari-eaton <BhavikKothari@eaton.com>

--- prerequisite_changes.py
+++ prerequisite_changes.py
@@ -1,4 +1,4 @@
-# This file is for making modification in edgex-TAF tests
+# This file is for making modification in EdgeX-TAF tests
 
 import sys
 import fileinput
@@ -10,7 +10,7 @@ module = sys.argv[2]
 def swap(f_path, search_str, new_str):
     with fileinput.FileInput(f_path, inplace=True) as file:
         for line in file:
-            if(line == search_str):
+            if line == search_str:
                 print(new_str, end='\n')
             else:
                 print(line, end='')
@@ -32,7 +32,9 @@ def global_variable(file_path):
         assert "BASE_URL" in content, print("-----BASE_URL does not exist in global_variables.py-----")
         swap(file_path, "BASE_URL = \"localhost\"\n", "BASE_URL = \"\"")
         assert "DEPLOY_TYPE" in content, print("-----DEPLOY_TYPE does not exist in global_variables.py-----")
-        swap(file_path, "DEPLOY_TYPE = \"docker\"\n", "DEPLOY_TYPE = os.getenv(\"DEPLOY_TYPE\")")
+        swap(file_path,
+             "DEPLOY_TYPE = \"docker\"\n",
+             "DEPLOY_TYPE = os.getenv(\"DEPLOY_TYPE\")")
 
 
 def change_auth_type(file_path):
@@ -43,7 +45,8 @@ def change_auth_type(file_path):
     with open(file_path, 'r') as f:
         newlines = []
         for line in f.readlines():
-            newlines.append(line.replace('Authorization=Bearer ${jwt_token}', 'X-Auth-Token=${jwt_token}'))
+            newlines.append(line.replace('Authorization=Bearer ${jwt_token}',
+                                         'X-Auth-Token=${jwt_token}'))
     with open(file_path, 'w') as f:
         for line in newlines:
             f.write(line)
@@ -96,31 +99,61 @@ def general_changes():
     global_variable("TAF/config/global_variables.py")
     change_auth_type("TAF/testCaseModules/keywords/setup/startup_checker.py")
     change_auth_type("TAF/testCaseModules/keywords/common/commonKeywords.robot")
-    add_data("TAF/testCaseModules/keywords/setup/startup_checker.py", "import subprocess", "import os" + "\n")
-    add_data("TAF/testCaseModules/keywords/setup/startup_checker.py", "def check_service_startup(d, token):", "    SettingsInfo().constant.BASE_URL=open(os.getenv(\"DATA_FILE\"), \"r\").read()\n    SettingsInfo().constant.BASE_URL=SettingsInfo().constant.BASE_URL[:-1]" + "\n")
+    add_data("TAF/testCaseModules/keywords/setup/startup_checker.py",
+             "import subprocess", "import os" + "\n")
+    add_data("TAF/testCaseModules/keywords/setup/startup_checker.py",
+             "def check_service_startup(d, token):",
+             "    SettingsInfo().constant.BASE_URL=open(os.getenv(\"DATA_FILE\"), \"r\").read()\n    SettingsInfo().constant.BASE_URL=SettingsInfo("
+             ").constant.BASE_URL[:-1]" + "\n")
     if module == "KVM" or module == "kvm":
-        add_data("TAF/testCaseModules/keywords/common/commonKeywords.robot", "Set suite variable  ${response}  ${resp.status_code}", "    Set suite variable  ${body}  ${resp.json()}\n")
+        add_data("TAF/testCaseModules/keywords/common/commonKeywords.robot",
+                 "Set suite variable  ${response}  ${resp.status_code}",
+                 "    Set suite variable  ${body}  ${resp.json()}\n")
 
 
 def main():
     general_changes()
     if service == "core-metadata":
-        # This is core-metadata service sprcific changes
+        # This is core-metadata service specific changes
         change_auth_type("TAF/testCaseModules/keywords/core-metadata/coreMetadataAPI.robot")
         change_port("8443/core-metadata", "8002/metadata")
         if module == "KVM" or module == "kvm":
-            add_data("TAF/testScenarios/functionalTest/V2-API/core-metadata/device/GET-Positive.robot", "When Query All Devices\n", "    ${length}=    Get Length    $body[device]\n")
-            add_data("TAF/testScenarios/functionalTest/V2-API/core-metadata/device/GET-Positive.robot", "When Query All Devices With offset=2\n", "    ${length}=    Get Length    $body[object]\n")
-            replace("TAF/testScenarios/functionalTest/V2-API/core-metadata/device/GET-Positive.robot", "== 7", "== ${length}")
-            replace("TAF/testScenarios/functionalTest/V2-API/core-metadata/device/GET-Positive.robot", "== 5", "== ${length}-2")
-            add_data("TAF/testScenarios/functionalTest/V2-API/core-metadata/deviceprofile/GET-Positive.robot", "When Query All Device Profiles\n", "    ${length}=    Get Length    $body[profiles]\n")
-            add_data("TAF/testScenarios/functionalTest/V2-API/core-metadata/deviceprofile/GET-Positive.robot", "When Query All Device Profiles With offset=2\n", "    ${length}=    Get Length    $body[object]\n")
-            replace("TAF/testScenarios/functionalTest/V2-API/core-metadata/deviceprofile/GET-Positive.robot", "== 8", "== ${length}")
-            replace("TAF/testScenarios/functionalTest/V2-API/core-metadata/deviceprofile/GET-Positive.robot", "== 6", "== ${length}")
-            add_data("TAF/testScenarios/functionalTest/V2-API/core-metadata/deviceservice/GET-Positive.robot", "When Query All Device Services\n", "    ${length}=    Get Length    $body[object]\n")
-            add_data("TAF/testScenarios/functionalTest/V2-API/core-metadata/deviceservice/GET-Positive.robot", "When Query All Device Services With offset=2\n", "    ${length}=    Get Length    $body[object]\n")
-            replace("TAF/testScenarios/functionalTest/V2-API/core-metadata/deviceservice/GET-Positive.robot", "== 6", "== ${length}-2")
-            replace("TAF/testScenarios/functionalTest/V2-API/core-metadata/deviceservice/GET-Positive.robot", "== 4", "== ${length}-4")
+            add_data("TAF/testScenarios/functionalTest/V2-API/core-metadata/device/GET-Positive.robot",
+                     "When Query All Devices\n",
+                     "    ${length}=    Get Length    $body[device]\n")
+            add_data("TAF/testScenarios/functionalTest/V2-API/core-metadata/device/GET-Positive.robot",
+                     "When Query All Devices With offset=2\n",
+                     "    ${length}=    Get Length    $body[object]\n")
+            replace("TAF/testScenarios/functionalTest/V2-API/core-metadata/device/GET-Positive.robot",
+                    "== 7",
+                    "== ${length}")
+            replace("TAF/testScenarios/functionalTest/V2-API/core-metadata/device/GET-Positive.robot",
+                    "== 5",
+                    "== ${length}-2")
+            add_data("TAF/testScenarios/functionalTest/V2-API/core-metadata/deviceprofile/GET-Positive.robot",
+                     "When Query All Device Profiles\n",
+                     "    ${length}=    Get Length    $body[profiles]\n")
+            add_data("TAF/testScenarios/functionalTest/V2-API/core-metadata/deviceprofile/GET-Positive.robot",
+                     "When Query All Device Profiles With offset=2\n",
+                     "    ${length}=    Get Length    $body[object]\n")
+            replace("TAF/testScenarios/functionalTest/V2-API/core-metadata/deviceprofile/GET-Positive.robot",
+                    "== 8",
+                    "== ${length}")
+            replace("TAF/testScenarios/functionalTest/V2-API/core-metadata/deviceprofile/GET-Positive.robot",
+                    "== 6",
+                    "== ${length}")
+            add_data("TAF/testScenarios/functionalTest/V2-API/core-metadata/deviceservice/GET-Positive.robot",
+                     "When Query All Device Services\n",
+                     "    ${length}=    Get Length    $body[object]\n")
+            add_data("TAF/testScenarios/functionalTest/V2-API/core-metadata/deviceservice/GET-Positive.robot",
+                     "When Query All Device Services With offset=2\n",
+                     "    ${length}=    Get Length    $body[object]\n")
+            replace("TAF/testScenarios/functionalTest/V2-API/core-metadata/deviceservice/GET-Positive.robot",
+                    "== 6",
+                    "== ${length}-2")
+            replace("TAF/testScenarios/functionalTest/V2-API/core-metadata/deviceservice/GET-Positive.robot",
+                    "== 4",
+                    "== ${length}-4")
     if service == "core-data":
         # TBD
         print("This is core-data service related changes")
